ed8b58f3f013764c959f51c330e34921f108866a
jaisy123 <banzesss05@gmail.com.>
2026-07-05 17:52:50 +0700

    cicd

--- lib/features/news/presentation/bloc/news_event.dart
+++ lib/features/news/presentation/bloc/news_event.dart
@@ -1,5 +1,5 @@
 import 'package:equatable/equatable.dart';
-import '../../domain/entities/news_entity.dart'
+import '../../domain/entities/news_entity.dart';
 
 abstract class NewsEvent extends Equatable {
   const NewsEvent();
